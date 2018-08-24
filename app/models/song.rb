5ca5b83162f535fef2525f87c06680fc23975f4c
Monica  Karasek <mlkarasek@gmail.com>
2018-08-24 23:58:15 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,5 +9,13 @@ class Song < ActiveRecord::Base
 
   def artist_name
     self.artist ? self.artist.name : nil
-  end 
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
 end

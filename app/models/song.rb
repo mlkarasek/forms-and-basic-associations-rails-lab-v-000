84bdbaaf7e1f97f65116ad10d8493fa273d78da8
Monica  Karasek <mlkarasek@gmail.com>
2018-08-25 00:03:10 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -19,7 +19,7 @@ class Song < ActiveRecord::Base
     self.genre ? self.genre.name : nil
   end
 
-  def note_contents=(name)
+  def note_contents=(contents)
     self.note = Note.find_or_create_by(content: content)
   end
 
